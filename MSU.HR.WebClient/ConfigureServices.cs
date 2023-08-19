d9f879d126c015196bebf641dc9c155fc99aba2e
Hendry Priyatno <hendry.priyatno@gmail.com>
2023-08-19 13:54:41 +0700

    success running

---
+++ MSU.HR.WebClient/ConfigureServices.cs
@@ -0,0 +1,49 @@
+﻿using MSU.HR.Contexts;
+using MSU.HR.Models.Entities;
+using MSU.HR.Services.Interfaces;
+using MSU.HR.Services.Repositories;
+
+namespace MSU.HR.WebClient
+{
+    public static class ConfigureServices
+    {
+        public static IConfiguration? Configuration { get; }
+        public static IServiceCollection AddWebClientServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DatabaseContext>(options =>
+            {
+            });
+
+            services
+                .AddIdentityCore<AspNetUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.User.RequireUniqueEmail = true;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = false;
+                })
+                .AddEntityFrameworkStores<DatabaseContext>();
+
+            services.AddAuthentication()
+                     .AddCookie(options =>
+                     {
+                         options.LoginPath = "/Auth/Login";
+                         options.LogoutPath = "/Auth/Logout";
+                     });
+
+
+            services.AddMvc();
+            //services.AddHttpContextAccessor();
+            //builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            //services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            //services.AddScoped<IUser, UserRepository>();
+
+
+            return services;
+        }
+    }
+}
--- MSU.HR.WebClient/Program.cs
+++ MSU.HR.WebClient/Program.cs
@@ -1,44 +1,10 @@
-using MSU.HR.Contexts;
-using MSU.HR.Models.Entities;
-using MSU.HR.Services.Interfaces;
-using MSU.HR.Services.Repositories;
-using Microsoft.Extensions.DependencyInjection.Extensions;
+using MSU.HR.WebClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<DatabaseContext>(options =>
-{
-});
-
-builder.Services
-    .AddIdentityCore<AspNetUser>(options =>
-    {
-        options.SignIn.RequireConfirmedAccount = false;
-        options.User.RequireUniqueEmail = true;
-        options.Password.RequireDigit = false;
-        options.Password.RequiredLength = 6;
-        options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequireUppercase = false;
-        options.Password.RequireLowercase = false;
-    })
-    .AddEntityFrameworkStores<DatabaseContext>();
-
-builder.Services.AddAuthentication()
-         .AddCookie(options =>
-         {
-             options.LoginPath = "/Auth/Login";
-             options.LogoutPath = "/Auth/Logout";
-         });
-//.ConfigureApplicationCookie(options => options.LoginPath = "/Auth/Login");
-
-builder.Services.AddMvc();
-//builder.Services.AddHttpContextAccessor();
-//builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-builder.Services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-builder.Services.AddScoped<IUser, UserRepository>();
+builder.Services.AddWebClientServices(builder.Configuration);
 
 var app = builder.Build();
 
